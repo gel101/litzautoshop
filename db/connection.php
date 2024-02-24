e28e750dc8ab8e8d2069f3e5fb31559512b2b2b1
gel101 <malano.angelo@dnsc.edu.ph>
2024-02-25 03:11:16 +0800

    change the database info

--- db/connection.php
+++ db/connection.php
@@ -1,7 +1,7 @@
 <?php  
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$db_name = "garagedb";
+$hostname = "srv1327.hstgr.io";
+$username = "u596510003_garage";
+$password = "4&Aq?5~euRj";
+$db_name = "u596510003_garagedb";
 
 $conn = mysqli_connect($hostname, $username, $password, $db_name);
--- pdf/connection.php
+++ pdf/connection.php
@@ -1,10 +1,11 @@
 <?php
 Class dbObj{
 	/* Database connection start */
-	var $dbhost = "localhost";
-	var $username = "root";
-	var $password = "";
-	var $dbname = "garagedb";
+	var $dbhost = "srv1327.hstgr.io";
+	var $username = "u596510003_garage";
+	var $password = "4&Aq?5~euRj";
+	var $dbname = "u596510003_garagedb";
+	
 	var $conn;
 	function getConnstring() {
 		$con = mysqli_connect($this->dbhost, $this->username, $this->password, $this->dbname) or die("Connection failed: " . mysqli_connect_error());
